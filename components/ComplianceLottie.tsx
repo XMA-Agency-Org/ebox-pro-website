eefc1f6ee377fcfdf9b967640a1da8371adc8cfd
itsdaxen <fa.rzam.sadegh@gmail.com>
2025-10-20 13:05:51 +0300

    Update ComplianceLottie component with new animation source URL

--- components/ComplianceLottie.tsx
+++ components/ComplianceLottie.tsx
@@ -6,7 +6,7 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 export const ComplianceLottie = () => {
   return (
     <DotLottieReact
-      src="https://lottie.host/a08b1a0b-ec98-44c0-8f3d-50d045a87822/Lw8L5XYBJx.lottie"
+      src="https://lottie.host/694d6f27-bda6-4eae-a705-02d1144b7d78/rPaJLyYby1.lottie"
       loop
       autoplay
     />
