17c195174f49c05be7eab7f911fe7545d4f73e8e
Faez <faez@xmaagency.com>
2025-12-29 19:50:35 +0400

    Adding Google Ads Tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WNVPDJ4Q');`}
         </Script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17763076208"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-17763076208');`}
+        </Script>
         <link
           rel="preload"
           as="video"
